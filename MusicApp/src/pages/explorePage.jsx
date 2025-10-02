c7b969c5ecff547264d34e3e43f85801dda3e466
Manavk2004 <mjkamdar04@gmail.com>
2025-10-02 13:39:50 -0400

    test

--- MusicApp/src/pages/explorePage.jsx
+++ MusicApp/src/pages/explorePage.jsx
@@ -21,6 +21,17 @@ export function ExplorePage(){
     const location = useLocation()
     console.log(location)
 
+    useEffect(() => {
+        console.log("The window", window.location.search)
+        const params = new URLSearchParams(window.location.search)
+        const accessToken2 = params.get("access_token")
+        console.log("Access token", accessToken2)
+        if(accessToken2 !== null){
+            setValue(accessToken2)
+        }
+        console.log("Here is the value", value)
+    }, [])
+
 
     //fetchRequests
     const getTopFiveSongs = async () =>{
