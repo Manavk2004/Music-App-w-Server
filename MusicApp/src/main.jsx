8450070899aa27d6a631f487f44e05b767c3e3c1
Manavk2004 <mjkamdar04@gmail.com>
2025-10-02 13:49:54 -0400

    test

--- MusicApp/src/main.jsx
+++ MusicApp/src/main.jsx
@@ -4,7 +4,10 @@ import './css/homepage.css'
 import './css/explorepage.css'
 import './css/savedPage.css'
 import { App } from './app.jsx'
+import { Provider } from 'jotai'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <Provider>
+        <App />
+    </Provider>
 )
